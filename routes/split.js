18f4a21f48ef00ba2bdeb999c90721347a3c3f1f
Prakhar Gupta <pguptaking@gmail.com>
2023-05-30 18:11:41 +0530

    send sse and error when filename not supplied in json

--- routes/split.js
+++ routes/split.js
@@ -7,8 +7,49 @@ app.get('/', (req, res)=>{
     log(req.body)
 })
 app.post('/', (req, res)=>{
-    log(req.body)
-
-    res.sendStatus(httpCode.ACCEPTED)
+    let code = Number(handleData(req.body))
+    log('code is', typeof code)
+    res.sendStatus(code)
 })
+
+function handleData(data){
+    log(data.name, "HI")
+    if (data.name === ''){
+        sendSse({'message':'filename not supplied'})
+        return httpCode.BAD_REQUEST
+    }
+    for (let d of data.split_info){
+        log(d.sp, d.ep)
+
+    }
+}
+function splitFile(sp, ep, filename){
+    if (process.platform == 'win32'){
+        log('windows')
+        let command = `split.bat `
+        log(command)
+        require('child_process').execSync(
+            command,
+            {
+                cwd: __dirname,
+                stdio: 'inherit'
+            }
+        )
+        return true;
+    } else if (process.platform == 'linux') {
+        log('other/linux')
+        let command = `./split.sh ${sp} ${ep}`
+        require('child_process').execSync(
+            command,
+            {
+                cwd: __dirname+"/upload/",
+                stdio: 'inherit'
+            }
+        )        
+        return true;
+    } else {
+        sse.sendSse('Unsupported server OS')
+        return false
+    }
+}
 module.exports = app
