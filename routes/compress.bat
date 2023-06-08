e5137499b07b5281426999769cd3a8fb7a6ac969
Prakhar Gupta <pguptaking@gmail.com>
2023-06-08 17:47:16 +0530

    windows script to compress pdf

---
+++ routes/compress.bat
@@ -0,0 +1,19 @@
+set home_dir=%~dp0
+set gswin64c_dir=%home_dir%\pdfbm\gs\bin
+set uploads_dir=%home_dir%\upload\
+
+cd %uploads_dir%
+gs.lnk  -q -dNOPAUSE -dBATCH -dSAFER \
+    -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.3 \
+    -dPDFSETTINGS=/screen \
+    -dEmbedAllFonts=true \
+    -dSubsetFonts=true \
+    -dColorImageDownsampleType=/Bicubic \
+    -dColorImageResolution=%3 \
+    -dGrayImageDownsampleType=/Bicubic \
+    -dGrayImageResolution=%3 \
+    -dMonoImageDownsampleType=/Bicubic \
+    -dMonoImageResolution=%3 \
+    -sOutputFile=%2 \
+     %1
